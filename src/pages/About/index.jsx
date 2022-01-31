4257aa5377d5d8835b2eec876ed713c6dd0e0edd
cdeguelle <75249048+cdeguelle@users.noreply.github.com>
2022-01-31 18:06:38 +0100

    update responsive design of about section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,7 +10,7 @@ const AboutWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     height: 80vh;
-    padding-top: 10vh;
+    padding-top: ${({ isMobile }) => (isMobile ? '' : '10vh')};
     scroll-margin-top: -10px;
 `
 
@@ -28,7 +28,7 @@ function About() {
     const isMobile = window.matchMedia('(max-width: 426px)').matches
 
     return (
-        <AboutWrapper id='about' theme={theme}>
+        <AboutWrapper id='about' theme={theme} isMobile={isMobile}>
             <Subtitle theme={theme} isMobile={isMobile}>A propos de moi</Subtitle>
             <TitleUnderline theme={theme} />
             <Text theme={theme} isMobile={isMobile}>Développeur front-end junior certifié après un an de formation chez OpenClassrooms. Lors de cette formation, j’ai eu dix projets à réaliser allant de l’intégration d’une maquette à la création d’un algorithme de recherche et en finissant par me spécialiser dans le framework javascript React.js. Tout ceci vous est présenté ici même, bonne visite.</Text>
